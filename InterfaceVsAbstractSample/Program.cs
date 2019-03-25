5f3d1ed2b09e4204db252122a2423a3b841d4e23
Simran <simran_palia@yahoo.co.in>
2019-03-25 14:11:14 +0800

    Commit 1,

--- InterfaceVsAbstractSample/Program.cs
+++ InterfaceVsAbstractSample/Program.cs
@@ -6,6 +6,53 @@ using System.Threading.Tasks;
 
 namespace InterfaceVsAbstractSample
 {
+    public abstract class BasicFunctionality
+    {
+        public abstract void Incoming();
+
+        public abstract void Outgoing();
+
+        public virtual void Keypad()
+        {
+
+        }
+    }
+    public class IPhone : BasicFunctionality, IDbHandshake
+    {
+        public void CreateConnectionString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Incoming()
+        {
+            //Apple style incoming
+        }
+
+        public override void Outgoing()
+        {
+            //Apple style outgoing
+        }
+
+        public void PassCredential()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TestConnection()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IDbHandshake
+    {
+        void CreateConnectionString();
+
+        void PassCredential();
+
+        void TestConnection();
+    }
     class Program
     {
         static void Main(string[] args)
